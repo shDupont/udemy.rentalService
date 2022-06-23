9c0deeb8377a581ffec64c56ee2a31cba6c1b5df
Letícia Dupont <leticia.dupont20@gmail.com>
2022-06-23 15:42:38 -0300

    Initial commit

---
+++ src/services/BrazilTaxService.java
@@ -0,0 +1,5 @@
+package entities;
+
+public class BrazilTaxService {
+    private Double tax;
+}
---
+++ src/services/RentalService.java
@@ -0,0 +1,6 @@
+package entities;
+
+public class RentalService {
+    private Double pricePerHour;
+    private Double pri
+}
